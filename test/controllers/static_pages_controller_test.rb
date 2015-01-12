62d0d286cb13478bf5188b1c124e364f62bfa869
Steve Yang <steveyang95@berkeley.edu>
2015-01-12 13:42:26 -0800

    I dont remember what I'm adding

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | MoneyManage"
+    assert_select "title", "MoneyManage"
   end
 
   test "should get help" do
